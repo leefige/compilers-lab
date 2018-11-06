1630a1d3f1e79a780649a0d1275b2359072a5753
leefige <liyifei15@mails.tsinghua.edu.cn>
2018-11-06 17:28:12 +0800

    Start working on null check.

--- exp2/src/submit/FindRedundantNullChecks.java
+++ exp2/src/submit/FindRedundantNullChecks.java
@@ -9,7 +9,7 @@ public class FindRedundantNullChecks {
      * This method should print out a list of quad ids of redundant null checks for each function.
      * The format should be "method_name id0 id1 id2", integers for each id separated by spaces.
      *
-     * @param args an array of class names. If "-e" presented, do extra analysing.
+     * @param _args an array of class names. If "-e" presented, do extra analysing.
      */
     public static void main(String[] _args) {
         List<String> args = new ArrayList<String>(Arrays.asList(_args));
@@ -17,5 +17,6 @@ public class FindRedundantNullChecks {
         if (extra)
             args.remove("-e");
         // TODO: Fill in this
+
     }
 }
